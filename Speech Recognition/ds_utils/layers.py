acfdfa9414b13ba27def5435d9401c4eb4f1f169
stellarator-x <admathur@iitk.ac.in>
2020-05-28 23:34:18 +0530

    file struct

---
+++ Speech Recognition/ds_utils/layers.py
@@ -0,0 +1,14 @@
+import tensorflow as tf 
+import numpy as np 
+import tensorflow.keras as keras
+
+class SeqBatchNorm(tf.keras.layers.Layer):
+
+    def __init__(self, num_outputs):
+        pass
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, input):
+        pass
